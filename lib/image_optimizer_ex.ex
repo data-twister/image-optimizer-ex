04cb3175f8573d0b99f49d2e31746b0d236898fb
mithereal <mithereal@gmail.com>
2022-09-14 01:55:06 -0700

    add more image optimizers

--- lib/image_optimizer_ex.ex
+++ lib/image_optimizer_ex.ex
@@ -5,8 +5,37 @@ defmodule ImageOptimizerEx do
     cond do
       Enum.member?([".jpg", ".jpeg"], Path.extname(src)) ->
         Optimizer.Jpeg.optimize(src, dst, options)
+
       Enum.member?([".png"], Path.extname(src)) ->
         Optimizer.Png.optimize(src, dst, options)
+    end
+  end
+
+  def quant(src, dst, options \\ %{}) do
+    cond do
+      Enum.member?([".png"], Path.extname(src)) ->
+        Optimizer.Png.quant(src, dst, options)
+    end
+  end
+
+  def optipng(src, dst, options \\ %{}) do
+    cond do
+      Enum.member?([".png"], Path.extname(src)) ->
+        Optimizer.Png.optipng(src, dst, options)
+    end
+  end
+
+  def trimage(src, dst, options \\ %{}) do
+    cond do
+      Enum.member?([".jpg", ".jpeg", ".png"], Path.extname(src)) ->
+        Optimizer.Trimage.optimize(src, dst, options)
+    end
+  end
+
+  def trimage(src, dst, options \\ %{}, :directory) do
+    cond do
+      Enum.member?([".jpg", ".jpeg", ".png"], Path.extname(src)) ->
+        Optimizer.Trimage.optimize_directory(src, dst, options)
     end
   end
 end
--- lib/optimizer/png.ex
+++ lib/optimizer/png.ex
@@ -3,8 +3,20 @@ defmodule ImageOptimizerEx.Optimizer.Png do
 
   def optimize(src, dst, options \\ %{}) do
     flags = cmd_flags(src, dst, options)
-    IO.inspect flags |> Enum.join(" ")
+    IO.inspect(flags |> Enum.join(" "))
     System.cmd("pngcrush", flags, stderr_to_stdout: true)
+  end
+
+  def quant(src, dst, options \\ %{}) do
+    flags = quant_cmd_flags(src, dst, options)
+    IO.inspect(flags |> Enum.join(" "))
+    System.cmd("pngquant", flags, stderr_to_stdout: true)
+  end
+
+  def optipng(src, dst, options \\ %{}) do
+    flags = optipng_cmd_flags(src, dst, options)
+    IO.inspect(flags |> Enum.join(" "))
+    System.cmd("pngquant", flags, stderr_to_stdout: true)
   end
 
   defp cmd_flags(src, dst, options) do
@@ -15,8 +27,36 @@ defmodule ImageOptimizerEx.Optimizer.Png do
       src,
       dst
     ]
-    |> Enum.filter(fn el -> el != nil end) 
-    |> List.flatten
+    |> Enum.filter(fn el -> el != nil end)
+    |> List.flatten()
+  end
+
+  defp quant_cmd_flags(src, dst, options) do
+    quality = options[:quality] || 80
+    speed = options[:speed] || 3
+
+    [
+      ["--quality", quality],
+      ["-speed", speed],
+      src,
+      dst
+    ]
+    |> Enum.filter(fn el -> el != nil end)
+    |> List.flatten()
+  end
+
+  defp optipng_cmd_flags(src, dst, options) do
+    o = options[:o] || 0
+    dir = options[:dir] || 3
+
+    [
+      ["-o", o],
+      ["-dir", dir],
+      src,
+      dst
+    ]
+    |> Enum.filter(fn el -> el != nil end)
+    |> List.flatten()
   end
 
   defp flag_verbose(options) do
